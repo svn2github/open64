cb2707bef4f3fd27ddded99b08b4c6a47a5cb716
dehao <dehao@ead328b1-e903-0410-a19d-eacee4b031f6>
2007-06-14 02:54:37 +0000

    A patch to r1094, r1094 should only be applied to IA64.
    git-svn-id: http://svn.open64.net/svnroot/open64@1100 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/be/cg/calls.cxx
+++ trunk/osprey/be/cg/calls.cxx
@@ -511,6 +511,7 @@ Generate_Entry (BB *bb, BOOL gra_run )
   ENTRYINFO *ent_info = ANNOT_entryinfo(ant);
   ST *st = ENTRYINFO_name(ent_info);	/* The entry's symtab entry */
 
+#ifdef TARG_IA64
   /* Add explicit definition of formal registers */
   if ( gra_run ) {
     FOR_ALL_BB_OPs (REGION_First_BB, op){
@@ -527,6 +528,7 @@ Generate_Entry (BB *bb, BOOL gra_run )
       }
     }
   }
+#endif
 
   if ((BB_rid(bb) != NULL) && ( RID_level(BB_rid(bb)) >= RL_CGSCHED )) {
   	/* don't change bb's which have already been through CG */
