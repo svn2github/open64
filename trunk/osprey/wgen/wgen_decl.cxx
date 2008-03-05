cd448c8dca76eabb979ea185d4bb8fdc45fe8fb8
dehao <dehao@ead328b1-e903-0410-a19d-eacee4b031f6>
2008-03-05 03:20:01 +0000

    If a function is marked as __attribute__ ((weak)) This function should not be marked as "EXPORT_PROTECTED" because it can be preempted by other extern functions with the same name.
    git-svn-id: http://svn.open64.net/svnroot/open64@1455 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/wgen/wgen_decl.cxx
+++ trunk/osprey/wgen/wgen_decl.cxx
@@ -1246,7 +1246,7 @@ WGEN_Start_Function(gs_t fndecl)
 	    gs_decl_namespace_scope_p (fndecl))))
 	eclass = EXPORT_INTERNAL; // bug 7550
       else if (gs_tree_public(fndecl) || gs_decl_weak(fndecl)) {
-	if (gs_decl_inline(fndecl) || !gs_decl_weak(fndecl))
+	if (gs_decl_inline(fndecl) && !gs_decl_weak(fndecl))
 	  eclass = EXPORT_PROTECTED;
 	else eclass = EXPORT_PREEMPTIBLE;
       }
