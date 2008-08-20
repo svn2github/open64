16c4774e037b124b1efab3c8ad1817534ac6769d
hjz <hjz@ead328b1-e903-0410-a19d-eacee4b031f6>
2008-08-20 16:51:23 +0000

    Revert the code to deal with OPR_ASHR in the function BITWISE_DCE::Redundent_cvl() to revision 1773, since the change by revision 1774 has some problem.
    git-svn-id: http://svn.open64.net/svnroot/open64@1810 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/merge08/osprey/be/opt/opt_bdce.cxx
+++ branches/merge08/osprey/be/opt/opt_bdce.cxx
@@ -1206,17 +1206,14 @@ BITWISE_DCE::Redundant_cvtl(BOOL sign_xtd, INT32 to_bit, INT32 from_bit,
 	return uval64 <= ((0x1ll << from_bit) - 1);
       }
 #endif
+#ifdef TARG_SL
     case OPR_ASHR:
-      //       I4I4LDID 0 <2,1,a>
-      //       I4INTCONST 24 (0x18)
-      //     I4ASHR
-      //   I4CVTL 8  <- reduntant CVTL
-      if (opnd->Opnd(1)->Kind() == CK_CONST) {
-        if (from_bit >= (MTYPE_size_min(dtyp) - opnd->Opnd(1)->Const_val())) {
-          return (MTYPE_signed(dtyp) == sign_xtd);
-        }
+      if (from_bit >= MTYPE_size_min(opnd->Dsctyp())) {
+        return TRUE;
+      } else {
+	return FALSE;
       }
-      return FALSE;
+#endif
     case OPR_EXTRACT_BITS:
       //     U4U4LDID 72 <1,4,.preg_U4>
       //   U4EXTRACT_BITS <bofst:27 bsize:4>
