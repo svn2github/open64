e584ab9e674da1c91c3e51d505f18cc76289841b
laijx <laijx@ead328b1-e903-0410-a19d-eacee4b031f6>
2008-09-26 03:54:35 +0000

    Enable VHO_Lower_Mstid handling UNION since we have fixed the wrong field_id in the last check-in.
    git-svn-id: http://svn.open64.net/svnroot/open64@1942 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/merge08/osprey/be/vho/vho_lower.cxx
+++ branches/merge08/osprey/be/vho/vho_lower.cxx
@@ -1903,10 +1903,6 @@ VHO_Lower_Mstid (WN * wn)
       return wn;
   }
 
-  // if type is union, disable lower it so far.
-  if (TY_is_union(src_ty_idx) || TY_is_union(dst_ty_idx) )
-      return wn;
-
   bytes = TY_size(dst_ty_idx);
 
 #if defined(TARG_NVISA)
