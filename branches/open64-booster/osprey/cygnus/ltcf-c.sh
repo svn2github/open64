57807f0e38bfb5c646bd1ed56cc8dc1c1ee35e38
dcoakley <dcoakley@ead328b1-e903-0410-a19d-eacee4b031f6>
2009-04-10 18:53:35 +0000

    r200@pcepinot:  dcoakley | 2009-04-10 12:02:07 -0700  Update copyright notices.
    git-svn-id: http://svn.open64.net/svnroot/open64@2240 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/open64-booster/osprey/cygnus/ltcf-c.sh
+++ branches/open64-booster/osprey/cygnus/ltcf-c.sh
@@ -2,6 +2,8 @@
 
 # ltcf-c.sh - Create a C compiler specific configuration
 #
+# Copyright (C) 2009 Advanced Micro Devices, Inc.  All Rights Reserved.
+#
 # Copyright (C) 1996-2000, 2001 Free Software Foundation, Inc.
 # Originally by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 #
