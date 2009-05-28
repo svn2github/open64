adbca6f87be3381adca5bceee116b72a03eaf51e
dcoakley <dcoakley@ead328b1-e903-0410-a19d-eacee4b031f6>
2009-05-28 05:23:06 +0000

    Fixed problem with automatically adding -lacml_mv when linking with g++, the preceding -lm needs to be doubled, otherwise the first -lm is dropped, since it is added later by g++.
    git-svn-id: http://svn.open64.net/svnroot/open64@2272 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/open64-booster/osprey/driver/phases.c
+++ branches/open64-booster/osprey/driver/phases.c
@@ -1927,8 +1927,14 @@ add_final_ld_args (string_list_t *args, phases_t ld_phase)
               // OSP -lm is needed
               add_library(args, "m");
 #ifdef TARG_X8664
-	      if (abi != ABI_N32)
+	      if (abi != ABI_N32) {
+		/* Sigh, g++ removes the first -lm since it is implicitly added by g++,
+		 * however adding two instances of -lm only removes 1.
+		 */
+                if (invoked_lang == L_CC)
+		  add_library(args, "m");
 		add_library(args, "acml_mv");
+              }
 #endif
 	    }
 	  }
