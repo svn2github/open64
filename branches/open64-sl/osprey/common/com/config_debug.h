a9eb3b674e09472f95d23573fc69bf6f57137c78
shenruifen <shenruifen@ead328b1-e903-0410-a19d-eacee4b031f6>
2009-11-20 09:56:25 +0000

    one file is ommited for last revert
    git-svn-id: http://svn.open64.net/svnroot/open64@2690 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/open64-sl/osprey/common/com/config_debug.h
+++ branches/open64-sl/osprey/common/com/config_debug.h
@@ -208,10 +208,6 @@ typedef struct debug_flags {
 #endif
 #ifdef TARG_SL
   BOOL stack_check;             /* Stack Check for SL */
-  struct option_list *dummy_chk_section_list; /* pad globals in designated sections with dummy variable to check overflow */
-  BOOL dummy_chk_seen; /* option explicit NOT set */
-  INT32 chk_blk_size_before; /* default size of a dummy block which is placed before st */
-  INT32 chk_blk_size_after; /* default size of a dummy block which is placed after st */
 #endif
   /* This buffer area allows references to new fields to be added in
    * later revisions, from other DSOs, without requiring a new be.so
@@ -324,12 +320,6 @@ extern DEBUG_FLAGS Initial_DEBUG;
 #define INSERT_END_FUNC_NAME   "__stackcheck_end"
 
 #define DEBUG_Stack_Check               (Current_DEBUG->stack_check)
-
-#define DEBUG_Dummy_Check_Sections        (Current_DEBUG->dummy_chk_section_list)
-#define DEBUG_Dummy_Check_Seen            (Current_DEBUG->dummy_chk_seen)
-#define DEBUG_Chk_Blk_Size_Before         (Current_DEBUG->chk_blk_size_before)
-#define DEBUG_Chk_Blk_Size_After         (Current_DEBUG->chk_blk_size_after)
-
 #endif
 
 
