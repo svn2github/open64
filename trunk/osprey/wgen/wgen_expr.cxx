cc3d79eabeb73e3783dc595dd5a7f73951a7cb51
shenruifen <shenruifen@ead328b1-e903-0410-a19d-eacee4b031f6>
2010-02-26 09:07:19 +0000

    Fix Bug 557 - bug in wgen:error CVTL bit Test case: struct S {   unsigned char b:3; } s;
    int
    main (void)
    {
      s.b = 4;
      if (s.b > 0 && s.b < 4)
        abort ();
      return 0;
    }
    
    
    
    git-svn-id: http://svn.open64.net/svnroot/open64@2868 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/wgen/wgen_expr.cxx
+++ trunk/osprey/wgen/wgen_expr.cxx
@@ -6395,7 +6395,7 @@ WGEN_Expand_Expr (gs_t exp,
 	  if (MTYPE_size_min(mtyp) < MTYPE_size_min(WN_rtype(wn))) {
 	    if (MTYPE_size_min(mtyp) != 32)
 	      wn = WN_CreateCvtl(OPR_CVTL, Widen_Mtype(mtyp), MTYPE_V,
-			         MTYPE_size_min(mtyp), wn);
+			         gs_type_type_precision(gs_tree_type(exp)), wn);
 	    else wn = WN_Cvt(WN_rtype(wn), mtyp, wn);
 	  }
 	  else {
