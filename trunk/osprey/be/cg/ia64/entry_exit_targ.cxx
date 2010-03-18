53700bba330170ea4b4cd078964bd2e621994682
ycwu <ycwu@ead328b1-e903-0410-a19d-eacee4b031f6>
2010-03-18 06:08:47 +0000

    another rotating register and output register overlap bug fix, for bug 561 revert the fixed in 2881, that patch can not solve this case.
    git-svn-id: http://svn.open64.net/svnroot/open64@2933 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/be/cg/ia64/entry_exit_targ.cxx
+++ trunk/osprey/be/cg/ia64/entry_exit_targ.cxx
@@ -142,12 +142,10 @@ EETARG_Fixup_Entry_Code (BB *bb)
 			ISA_REGISTER_CLASS_integer);
 		num_rotating = REGISTER_Number_Stacked_Rotating(
 			ISA_REGISTER_CLASS_integer);
-		// make sure num_rotating <= num_local+num_output
-		num_local = MAX(num_local, num_rotating - num_output);
-		INT max_rotating = REGISTER_Last_Rotating_Registers(ISA_REGISTER_CLASS_integer) - REGISTER_First_Rotating_Registers(ISA_REGISTER_CLASS_integer) + 1;
-		if ( num_rotating > num_local && num_rotating != max_rotating )
-		  FmtAssert(FALSE, ("Bug: rotating register and output register are overlap but no need to be. local=%d,out=%d,rotate=%d,max_rotate=%d",
-				num_local,num_output,num_rotating,max_rotating));
+		// set num_rotating not larg than num_local to prevent function
+		// call parameter error.
+		num_local = MAX(num_local, num_rotating);
+
 		if (num_local + num_output + num_rotating == 0) {
 			BB_Remove_Op (bb, op);
 		}
