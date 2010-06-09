3d7c16785b545eef76bdcd300aeeba605503892b
dcoakley <dcoakley@ead328b1-e903-0410-a19d-eacee4b031f6>
2010-06-09 22:51:16 +0000

    Create x86 Open64 4.2.4 release branch.
    git-svn-id: http://svn.open64.net/svnroot/open64@3222 ead328b1-e903-0410-a19d-eacee4b031f6

---
+++ branches/x86_open64-4.2.4/osprey/targia32_ppc32/driver/version_bk.c
@@ -0,0 +1,7 @@
+#include "version.h"
+const char *const cset_rev = "unknown";
+const char *const cset_key = "unknown";
+const char *const build_root = "unknown";
+const char *const build_host = "tu069003.ip.tsinghua.edu.cn";
+const char *const build_user = "root";
+const char *const build_date = "2007-08-30 21:25:09 +0800";
