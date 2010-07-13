24ce21aa37867179e5ae286c362b5954b634eee6
yangkeqiao <yangkeqiao@ead328b1-e903-0410-a19d-eacee4b031f6>
2010-07-13 09:55:52 +0000

    del temp files, wopt.sh and build.sh
    git-svn-id: http://svn.open64.net/svnroot/open64@3281 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/open64-ssa/wopt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /proj/opensrc_nobackup/zhiweic/SVN/puretrunk/osprey/targia32_x8664/wopt
-
-make BUILD_OPTIMIZE=DEBUG MACHINE_TYPE=i386 || exit 1
-
-cd /proj/opensrc_nobackup/zhiweic/SVN/puretrunk/
-
-make install MACHINE_TYPE=i386
