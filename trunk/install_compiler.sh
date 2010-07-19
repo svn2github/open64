8bb4208c3b7fe4b3cc6dca4b8e88350b16e1b56f
ycwu <ycwu@ead328b1-e903-0410-a19d-eacee4b031f6>
2010-07-19 01:46:34 +0000

    1. do not build 64bit library in i386 system 2. do not build share library in IA64 system, there is some problem in building share library 3. install the libstdc++.so.6 into a different directory other than be.so, or the compiler will issue an compatible problem.
    git-svn-id: http://svn.open64.net/svnroot/open64@3284 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/install_compiler.sh
+++ trunk/install_compiler.sh
@@ -206,12 +206,13 @@ INSTALL_GCC () {
 
     # Make links to gcc runtime libraries
     cd ${ROOT}
+    mkdir $PHASEPATH/64
     if [ "$TARG_HOST" = "x8664" ]
     then
 	for i in open64-gcc-4.2.0/lib64/lib*.so*; do
 	    if [ -e "$i" ]
 	    then
-		(cd $PHASEPATH; ln -sf ../../../../$i `basename $i`)
+		(cd $PHASEPATH/64; ln -sf ../../../../../$i `basename $i`)
 	    fi
 	done
 	for i in open64-gcc-4.2.0/lib/lib*.so*; do
@@ -225,7 +226,7 @@ INSTALL_GCC () {
 	for i in open64-gcc-4.2.0/lib/lib*.so*; do
 	    if [ -e "$i" ]
 	    then
-		(cd $PHASEPATH; ln -sf ../../../../$i `basename $i`)
+		(cd $PHASEPATH/64; ln -sf ../../../../../$i `basename $i`)
 	    fi
 	done
     fi
