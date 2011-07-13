0dcb9153a6c79bb7332c0522aa62bc1dfa53d572
laijx <laijx@ead328b1-e903-0410-a19d-eacee4b031f6>
2011-07-13 02:39:29 +0000

    Re-patch rev 3688 and re-fix bug 830.
    Add_idef() is used for both ILOAD and ISTORE. For ISTORE, the lbase is NULL but previous patch doesn't check that. Also, previous patch doesn't work for store to constant address like:
    *(int*)0xffffffffffff8001 = val;
    
    The new patch checks if the lbase or sbase is NULL and works for both ILOAD/ISTORE to constant address.
    
    Approved by Sun Chan.
    
    
    
    git-svn-id: http://svn.open64.net/svnroot/open64@3689 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/be/opt/opt_htable.cxx
+++ trunk/osprey/be/opt/opt_htable.cxx
@@ -2534,9 +2534,13 @@ CODEMAP::Add_idef(OPCODE opc, OCC_TAB_ENTRY *occ, STMTREP *stmt,
   CODEREP          *retv;
   OPERATOR         oper = OPCODE_operator(opc);
 
-  // make sure the dtyp for address const is Pointer_type
-  if (lbase->Kind() == CK_CONST && lbase->Dtyp() != Pointer_type)
+  // make sure the dtyp for constant base is Pointer_type
+  if (lbase != NULL && lbase->Kind() == CK_CONST 
+      && lbase->Dtyp() != Pointer_type)
     lbase->Set_dtyp_strictly(Pointer_type);
+  if (sbase != NULL && sbase->Kind() == CK_CONST 
+      && sbase->Dtyp() != Pointer_type)
+    sbase->Set_dtyp_strictly(Pointer_type);
 
   cr->Init_ivar(opc, dtyp, occ, dsctyp, lodty, lbase, sbase,
 		ofst, size, field_id);
