ef9e0bd64be34b715c3ea1dc7e3ab50eb4e8b99a
codestr0m <codestr0m@ead328b1-e903-0410-a19d-eacee4b031f6>
2011-08-02 05:59:16 +0000

    Removing misleading old branches
    git-svn-id: http://svn.open64.net/svnroot/open64@3706 ead328b1-e903-0410-a19d-eacee4b031f6

--- branches/pathscale-2.2.1/kpro64/kg++fe/rt_symtab.h
+++
@@ -1,5 +0,0 @@
-#ifndef rt_symtab_INCLUDED
-#define rt_symtab_INCLUDED
-
-extern ST_IDX Throw_Runtime_st_idx();
-#endif /* rt_symtab_INCLUDED */
--- branches/pathscale-2.2.1/kpro64/libF77/signal1.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2004 PathScale, Inc.  All Rights Reserved.
- */
-
-/* The g77 implementation of libf2c directly includes signal1.h0,
-   instead of copying it to signal1.h, since that seems easier to
-   cope with at this point.  */
-
-#include "signal1.h0"
