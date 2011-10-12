svn2github/open64

commit 78541549eae8fd9f681176100b18f4ff2fe6cc32

Fixed to bug 771.
(long double)-0.0 should not have the same value number of (long double)0.0.

Approved by Jian-Xin.


git-svn-id: http://svn.open64.net/svnroot/open64@3753 ead328b1-e903-0410-a19d-eacee4b031f6

trunk/osprey/be/opt/opt_vn_expr.cxx
@@ -651,6 +651,9 @@ VN_LITERAL_EXPR::is_equal_to(CONST_PTR expr) const
 	 else if (this_mty == MTYPE_F4 && 
 	          TCON_R4(other_tcon) == 0 && TCON_R4(_tcon) == 0)
 	    truth = TCON_word0(other_tcon) == TCON_word0(_tcon);
+	 else if (this_mty == MTYPE_F10 && 
+	          TCON_R10(other_tcon) == 0 && TCON_R10(_tcon) == 0)
+	    truth = TCON_v2(other_tcon) == TCON_v2(_tcon);
 #endif
          else {
 	    BOOL folded;

commit 31dc84eb2087ca65e0b1658cd09346216cee1d73

Fix bug880. Fix open64 build failure caused by r3753.
Code Review: Gilmore, Doug


git-svn-id: http://svn.open64.net/svnroot/open64@3759 ead328b1-e903-0410-a19d-eacee4b031f6

trunk/osprey/be/opt/opt_vn_expr.cxx
@@ -645,15 +645,10 @@ VN_LITERAL_EXPR::is_equal_to(CONST_PTR expr) const
 	 // fix bug 2691: when both are floating-point zero, use integer
 	 // comparison so +0 and -0 will be regarded as different for value
 	 // numbering purpose
-	 else if (this_mty == MTYPE_F8 && 
-	          TCON_R8(other_tcon) == 0 && TCON_R8(_tcon) == 0)
-	    truth = TCON_v1(other_tcon) == TCON_v1(_tcon);
-	 else if (this_mty == MTYPE_F4 && 
-	          TCON_R4(other_tcon) == 0 && TCON_R4(_tcon) == 0)
-	    truth = TCON_word0(other_tcon) == TCON_word0(_tcon);
-	 else if (this_mty == MTYPE_F10 && 
-	          TCON_R10(other_tcon) == 0 && TCON_R10(_tcon) == 0)
-	    truth = TCON_v2(other_tcon) == TCON_v2(_tcon);
+	 else if (Targ_Is_Zero(other_tcon))
+            truth = Targ_Is_Zero(_tcon);
+	 else if (Targ_Is_Neg_Zero(other_tcon))
+            truth = Targ_Is_Neg_Zero(_tcon);
 #endif
          else {
 	    BOOL folded;

trunk/osprey/common/com/MIPS/targ_const.cxx
@@ -4636,6 +4636,23 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/NVISA/targ_const.cxx
@@ -4902,6 +4902,23 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/SL/targ_const.cxx
@@ -4640,6 +4640,23 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/ia64/targ_const.cxx
@@ -5152,6 +5152,26 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    case MTYPE_F10:
+      return (TCON_R16(t) == 0.0 &&
+        (TCON_v0(t)|TCON_v1(t)|TCON_v2(t)|TCON_v3(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/loongson/targ_const.cxx
@@ -4861,6 +4861,23 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/ppc32/targ_const.cxx
@@ -5104,6 +5104,26 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    case MTYPE_F10:
+      return (TCON_R16(t) == 0.0 &&
+        (TCON_v0(t)|TCON_v1(t)|TCON_v2(t)|TCON_v3(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *

trunk/osprey/common/com/targ_const.h
@@ -482,6 +482,7 @@ extern TCON Quad_Zero_Tcon;
 
 /* Is the given constant a zero? */
 extern BOOL Targ_Is_Zero ( TCON t );
+extern BOOL Targ_Is_Neg_Zero ( TCON t );
 
 /* Determine whether a TCON represents an integral value, and if so
  * return its value in *iv:

trunk/osprey/common/com/x8664/targ_const.cxx
@@ -5730,6 +5730,26 @@ BOOL Targ_Is_Zero ( TCON t )
   }
   return FALSE;
 } /* Targ_Is_Zero */
+
+/*----------------------------------------------------------------------------
+ * return TRUE if the target representation of this TCON is negative zero
+ *--------------------------------------------------------------------------*/
+BOOL Targ_Is_Neg_Zero ( TCON t )
+{
+  switch (TCON_ty(t)) {
+    case MTYPE_F4:
+      return (TCON_R4(t) == 0.0 && TCON_v0(t) != 0);
+    case MTYPE_F8:
+      return (TCON_R8(t) == 0.0
+        && (TCON_v0(t)|TCON_v1(t)) != 0);
+    case MTYPE_F10:
+      return (TCON_R10(t) == 0.0 &&
+        (TCON_v0(t)|TCON_v1(t)|TCON_v2(t)|TCON_v3(t)) != 0);
+    default:
+      return FALSE;
+  }
+}
+
 
 /* ====================================================================
  *