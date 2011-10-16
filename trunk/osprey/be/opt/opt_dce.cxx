d62a1122cd7371b3996d513ca644da6e3f8644c5
shivaram <shivaram@ead328b1-e903-0410-a19d-eacee4b031f6>
2011-10-16 10:12:14 +0000

    Fix for bug#787. Removal of identity assignment statements(i=i) is prevented if lhs is not propagatable. CR by Ye,Mei and Gautam
    git-svn-id: http://svn.open64.net/svnroot/open64@3764 ead328b1-e903-0410-a19d-eacee4b031f6

--- trunk/osprey/be/opt/opt_dce.cxx
+++ trunk/osprey/be/opt/opt_dce.cxx
@@ -2149,7 +2149,8 @@ DCE::Required_stmt( const STMTREP *stmt ) const
 
   if (OPERATOR_is_scalar_store (opr) &&
       Enable_identity_removal() &&
-      stmt->Is_identity_assignment_removable())  // if COPYPROP assumes the stmt is deleted
+      stmt->Is_identity_assignment_removable() &&
+      !(stmt->Lhs()->Flags() & CF_DONT_PROP))  // if COPYPROP assumes the stmt is deleted
     return FALSE;
 
   // statements with zero-version chi nodes are required
@@ -3262,7 +3263,8 @@ DCE::Mark_statement_live( STMTREP *stmt ) const
 
   if (OPERATOR_is_scalar_store (opr) &&
       Enable_identity_removal() &&
-      stmt->Is_identity_assignment_removable()) {
+      stmt->Is_identity_assignment_removable() &&
+      !(stmt->Lhs()->Flags() & CF_DONT_PROP)) {
     // process the rhs expression, if any
     CODEREP *rhs = stmt->Rhs();
     if ( rhs != NULL ) {
